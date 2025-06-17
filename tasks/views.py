8906e4bc3329515071c38b588685c62df83970ea
Gogo-Dev <lic.adolfoesquivel@gmail.com>
2025-06-16 23:27:11 -0600

    Creacion del csrf token dentro del formulario

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def helloworld (request):
-    return HttpResponse ('hello World')
+    return render(request, 'signup.html',{
+        'from': UserCreationForm
+})
+
