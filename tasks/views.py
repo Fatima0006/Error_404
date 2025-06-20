4949d03e33ad3617d9b816abad00a9bfb50e5b19
Gogo-Dev-Lap <lic.adolfoEsquivel@gmail.com>
2025-06-19 19:01:37 -0600

    Actualizacion singup y verificacion de usuario

--- tasks/views.py
+++ tasks/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 
-def  home (request):
-    return render(request, 'home.html',{
-        
-})
-
-def  signup (request):
-
-    if request.method == 'GET': 
-        print('enviando Formulario')
+def  home(request):
+    return render(request, 'home.html')
+def signup(request):
+    if request.method == 'GET':
+        print('Enviando formulario')
+        return render(request, 'signup.html', {'form': UserCreationForm()})
+    
+    # POST
+    if request.POST['password1'] == request.POST['password2']:
+        try:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1']
+            )
+            user.save()
+            return HttpResponse('Usuario creado correctamente')
+        except:
+            return HttpResponse('Usuario ya existente')
     else:
-        print ('obteniendo datos') 
-        print(request.POST)
-
-    return render(request, 'signup.html',{
-        'from': UserCreationForm
-})
+        return HttpResponse('Las contraseñas no coinciden')
