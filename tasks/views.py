f9eafac4d088801e880d77a17c7c9cf9f29ef958
Gogo-Dev <lic.adolfoesquivel@gmail.com>
2025-06-27 19:21:32 -0600

    singout completado

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
@@ -31,11 +31,24 @@ def signup(request):
 def check_in(request):
     return render(request, 'check_in.html')
 
+
 def tasks(request):
-    return render(request,'tasks.html')#a
+    return render(request, 'tasks.html')  # a
+
 
 def signout(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('home')
+    logout(request)
     return render(request, 'home.html', {"error": "You are not logged in."})
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {"form": AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST["username"], password=request.POST["password"])
+        if user is None:
+            return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username and password did not match."})
+        else:
+            login(request, user)
+            return redirect('tasks')
