48a595f9babd525c332f343f215a133a06662590
Gogo-Dev <lic.adolfoesquivel@gmail.com>
2025-07-27 10:41:46 -0600

    Creacion de admin con los modelos

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task, Evento, Asistente
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(Evento)
+admin.site.register(Asistente)
+# Almacena la relación entre asistentes y eventos, incluyendo la fecha de registro.
