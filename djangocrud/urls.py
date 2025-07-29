30e896def167ff0a66e7dca0f60d326a83a2693d
Gogo-Dev <lic.adolfoesquivel@gmail.com>
2025-07-28 21:31:00 -0600

    Creacion de Ursl.py y views.py para task and event creations

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path ('signup/', views.signup, name='signup'),
     path('check_in/',views.check_in,name='check_in'),
     path('tasks/',views.tasks,name='tasks'),
+    path('task/create/',views.create_task,name= 'create_task'),
+    path('event/create/',views.create_event,name='create_event'),
     path('logout/',views.signout,name='logout'),
     path('signin/',views.signin,name='signin'),
 ]
